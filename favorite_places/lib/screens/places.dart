ee639ae889e098ac92266498bc74801deef0a7f9
tauseefsheikh01 <tauseef150198@gmail.com>
2023-09-12 17:52:25 +0530

    Favorite Places

--- favorite_places/lib/screens/places.dart
+++ favorite_places/lib/screens/places.dart
@@ -1,23 +1,35 @@
-import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PlacesScreen extends StatelessWidget {
+import 'package:favorite_places/screens/add_place.dart';
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:favorite_places/providers/user_places.dart';
+
+class PlacesScreen extends ConsumerWidget {
   const PlacesScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Places'),
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
           ),
         ],
       ),
-      body: const PlacesList(
-        places: [],
+      body: PlacesList(
+        places: userPlaces,
       ),
     );
   }
